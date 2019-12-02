e68530806ce964e08e90586d14bc0dfd4a9d000c
Melanie Balaz <melanie.j.balaz@gmail.com>
2019-12-02 18:50:27 +0100

    Adjust window size

--- signalprocessing/amplitude/amplitude_extraction.py
+++ signalprocessing/amplitude/amplitude_extraction.py
@@ -6,28 +6,28 @@ import matplotlib.pyplot as plt
 # https://docs.scipy.org/doc/numpy/reference/generated/numpy.hanning.html
 # https://docs.scipy.org/doc/scipy/reference/tutorial/fftpack.html
 brain_freq_bands = {
-                    'delta': (1, 4),
-                    'theta': (4, 8),
-                    'alpha': (8, 12),
-                    'beta': (12, 30),
-                    'gamma': (30, 45)
-                    }
+    'delta': (1, 4),
+    'theta': (4, 8),
+    'alpha': (8, 12),
+    'beta': (12, 30),
+    'gamma': (30, 45)
+}
 
-WINDOW_SIZE = 1.
+WINDOW_SIZE = 10.
 SAMPLING_RATE = 500
 FREQ_RESOLUTION = 1. / WINDOW_SIZE
 WINDOW_FUNCTION = scipy.signal.hann(M=int(WINDOW_SIZE * SAMPLING_RATE), sym=False)
 
 
 def apply_window_function(signal, window_function):
-    # build a matrix for window function
     return signal * window_function
 
 
 def fourier_transform(windowed_signal):
-    return 10*np.log10(np.fft.fft(windowed_signal))
+    return 10 * np.log10(np.fft.fft(windowed_signal))
 
 
+# pass whole spectrum for all channels to this function
 def avg_band_amplitude(spectrum, lower_limit, upper_limit):
     frequency_band = spectrum[int(lower_limit / FREQ_RESOLUTION):int(upper_limit / FREQ_RESOLUTION)]
     return np.mean(np.absolute(frequency_band))
@@ -35,11 +35,11 @@ def avg_band_amplitude(spectrum, lower_limit, upper_limit):
 
 def extract_amplitudes(input_signal):
     windowed_signal = apply_window_function(input_signal, WINDOW_FUNCTION)
-    frequency_spectrum = fourier_transform(windowed_signal)
-    plt.plot(10*np.log10(np.absolute(frequency_spectrum)))
+    frequency_spectrum = fourier_transform(windowed_signal).T
+    plt.plot(np.absolute(frequency_spectrum))
     plt.show()
     amplitudes_all_channels = []
-    for channel in frequency_spectrum.T[0:1]:
+    for channel in frequency_spectrum[0:1]:
         amplitudes = []
         for wave, band_range in brain_freq_bands.items():
             amplitudes.append(avg_band_amplitude(channel, band_range[0], band_range[1]))
@@ -50,11 +50,12 @@ def extract_amplitudes(input_signal):
 def main():
     raw = mne.io.read_raw_brainvision('../data/20191104_Cybathlon_Test_1.vhdr')
     # loads data from 0 to 1 sec
-    t_idx = raw.time_as_index([100., 300.])
+    t_idx = raw.time_as_index([100., 110.])
     data, times = raw[:, t_idx[0]:t_idx[1]]
-    plt.plot(extract_amplitudes(data))
+    amplitudes = extract_amplitudes(data)
+    plt.plot(amplitudes)
     plt.show()
-
+    print(amplitudes)
 
 # time = np.linspace(0, 0.5, 500)
 # input_signal = np.sin(40 * 2 * np.pi * time) + 0.5 * np.sin(90 * 2 * np.pi * time)
