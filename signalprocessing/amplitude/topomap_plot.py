a32340da8f4d8290c560bf9d85b08adc18a4891f
Melanie Balaz <melanie.j.balaz@gmail.com>
2019-12-04 15:18:05 +0100

    Add topomap plotting

--- signalprocessing/amplitude/topomap_plot.py
+++ signalprocessing/amplitude/topomap_plot.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 import mne
 
 
+def plot_data_for_single_channel(data_for_channel, raw):
+    #raw.rename_channels({'O9': 'I1', 'O10': 'I2'})
+    montage = mne.channels.make_standard_montage("standard_1020")
+    raw.set_montage(montage)
+    #raw.rename_channels({'I1': 'O9', 'I2': 'O10'})
+    # Remove bad channels from analysis
+    #raw.info['bads'] = ['F2', 'FFC2h', 'POO10h', 'O2']
+    raw.set_eeg_reference(ref_channels='average')
+    picks = mne.pick_types(raw.info, eeg=True, stim=False, exclude='bads')
+    pos = pos_from_raw(raw.info, picks)
+    plot_single_topomap(data_for_channel, pos, title='', cmap_rb=True)
+
+
 def plot_single_topomap(data, pos, vmin=None, vmax=None, title=None, cmap_rb=False):
     vmin = np.min(data) if vmin is None else vmin
     vmax = np.max(data) if vmax is None else vmax
@@ -16,18 +29,4 @@ def plot_single_topomap(data, pos, vmin=None, vmax=None, title=None, cmap_rb=Fal
                                               size="10%", format='%3.3f')
     cbar.set_ticks((vmin, vmax))
     ax.set_title(title)
-    # Load data, set montage and remove bad channels as before
-    # Load data
-    raw = mne.io.read_raw_brainvision('../data/20191104_Cybathlon_Test_1.vhdr')
-    # Set montage (location of channels)
-    raw.rename_channels({'O9': 'I1', 'O10': 'I2'})
-    montage = mne.channels.read_montage("standard_1020")
-    raw.set_montage(montage)
-    raw.rename_channels({'I1': 'O9', 'I2': 'O10'})
-    # Remove bad channels from analysis
-    raw.info['bads'] = ['F2', 'FFC2h']
-    picks = mne.pick_types(raw.info, eeg=True, stim=False, exclude='bads')
-    # Do something with your data per channel and get one number per channel as result (as 1D np array)
-    some_data_per_channel = np.arange(124)
-    pos = pos_from_raw(raw.info, picks)
-    plot_single_topomap(some_data_per_channel, pos, title='')
+    plt.savefig('brain.png')
