44c116ffa681a9663513038fbc7efb38deacbc25
biropost <billie.postlmayr@gmail.com>
2020-02-19 11:53:07 +0100

    some last optimizations

--- signalprocessing/signal_tools.py
+++ signalprocessing/signal_tools.py
@@ -1,3 +1,24 @@
-import peakfrequency.peak_frequency as fm
-import amplitude.amplitude_extraction_welch as am
+import numpy as np
+import scipy
+
+# Calculates an avg of the power within the given indexes
+def avg_band_amplitude(power, lower_limit_index, upper_limit_index):
+    range = power[:, lower_limit_index:upper_limit_index]
+    return np.mean(range, axis=1)
+
+
+# Returns for each brain wave bandwidth the average amplitude within that bandwidth for each electrode
+def extract_amplitudes(data):
+    frequencies, power = calculate_psd(data)
+    rescaled_power = 10 * np.log10(power)
+    amplitudes = []
+    for wave, band_range in brain_freq_bands.items():
+        lower_index = next(index for index, value in enumerate(frequencies) if value > band_range[0])
+        upper_index = next(index for index, value in enumerate(frequencies) if value > band_range[1])
+        amplitudes.append(avg_band_amplitude(rescaled_power, lower_index, upper_index))
+    return amplitudes
+
+
+def calculate_psd(input_signal):
+    return scipy.signal.welch(x=input_signal, fs=SAMPLING_RATE)
 
