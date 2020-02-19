025b0eea43f6e1863605a971c2c8bc1a6b9dd424
biropost <billie.postlmayr@gmail.com>
2020-02-19 11:11:30 +0100

    some last optimizations

--- signalprocessing/peakfrequency/peak_frequency.py
+++ signalprocessing/peakfrequency/peak_frequency.py
@@ -27,8 +27,23 @@ class PeakFrequency:
             self.bands = {'delta': (1, 4), 'theta': (4, 8), 'alpha': (8, 12), 'beta': (12, 30), 'gamma': (30, 45)}
         else:
             self.bands = bands
+        for band in self.bands:
+            from_val = int(self.samples / self.fs * self.bands[band][0])
+            to_val = int(self.samples / self.fs * self.bands[band][1])
+            self.bands[band] = (from_val, to_val)
 
-    def fit(self, x):
+    def peak_method(self, signal, method='median'):
+        inst_phase = np.unwrap(np.angle(signal))
+        inst_freq = np.diff(inst_phase, axis=0) / (2 * np.pi) * self.fs
+        if method is 'median':
+            median = np.median(inst_freq, axis=0)
+            return median
+        if method is 'rms':
+            rms = np.sqrt(np.median(inst_freq**2, axis=0))
+            return rms
+        return inst_freq
+
+    def transform_dict(self, x, method='median'):
         x = np.asarray(x)
         if x.shape[0] != self.samples and x.shape[1] != self.channels:
             raise ValueError("configs (", self.channels, ",", self.samples, ") do not match input dims ", x.shape)
@@ -38,31 +53,26 @@ class PeakFrequency:
         instant_frequency = dict()
         for band in self.bands:
             signal = np.zeros((self.samples, self.channels), dtype=complex)
-            from_val = int(self.samples / self.fs * self.bands[band][0])
-            to_val = int(self.samples / self.fs * self.bands[band][1])
+            from_val = self.bands[band][0]
+            to_val = self.bands[band][1]
             signal[from_val:to_val, :] = H[from_val:to_val, :]
             signal = signal.T.dot(self.idft).T
-            inst_phase = np.unwrap(np.angle(signal))
-            inst_freq = np.diff(inst_phase, axis=0) / (2 * np.pi) * self.fs
-            instant_frequency[band] = np.median(inst_freq, axis=0)
-        # returns a dict for each frequency band containing a channel vector for each electrode's median inst freq
+            instant_frequency[band] = self.peak_method(signal, method)
         return instant_frequency
 
-    def peaks_vector(self, x):
+    def transform(self, x, method='median'):
         x = np.asarray(x)
         if x.shape[0] != self.samples and x.shape[1] != self.channels:
             raise ValueError("configs (", self.channels, ",", self.samples, ") do not match input dims ", x.shape)
         if np.iscomplexobj(x):
             raise ValueError("x is not a real signal.")
         H = self.dft.dot(x) * self.hilbert.T
-        instant_frequency = dict()
+        instant_frequency = []
         for band in self.bands:
             signal = np.zeros((self.samples, self.channels), dtype=complex)
             from_val = int(self.samples / self.fs * self.bands[band][0])
             to_val = int(self.samples / self.fs * self.bands[band][1])
             signal[from_val:to_val, :] = H[from_val:to_val, :]
             signal = signal.T.dot(self.idft).T
-            inst_phase = np.unwrap(np.angle(signal))
-            inst_freq = np.diff(inst_phase, axis=0) / (2 * np.pi) * self.fs
-            instant_frequency[band] = inst_freq
+            instant_frequency.append(self.peak_method(signal, method))
         return instant_frequency
---
+++ signalprocessing/signal_tools.py
@@ -0,0 +1,3 @@
+import peakfrequency.peak_frequency as fm
+import amplitude.amplitude_extraction_welch as am
+
