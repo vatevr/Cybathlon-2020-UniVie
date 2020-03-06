aa0c232de71d02803a4f82e8b85908450f7e69fa
MatthaeusHilpold <matthaeus.hilpold@gmail.com>
2020-03-06 15:00:33 +0100

    commit before merge

--- Pipeline/src/tools/testingUtility.py
+++ Pipeline/src/tools/testingUtility.py
@@ -1,9 +1,16 @@
 from Pipeline.src.classifier.riemannianClassifier.classifier import riemannianClassifier
 import numpy as np
+from pylsl import StreamInfo, StreamOutlet
 
 clfpath = "../savedFilters/test"
 clf = riemannianClassifier(savePath=clfpath)
 clf.load_self()
+
+#Set LSL
+LSLchannels = 4
+LSLinterval = 10
+LSLinfo = StreamInfo('Feedback', 'VIS', LSLchannels, LSLinterval, 'float32', 'myuid34234')
+LSLoutlet = StreamOutlet(LSLinfo)
 
 def printWindowCallback(window):
     print(window.shape)
@@ -13,3 +20,11 @@ def printClassifierProbaCallback(window):
     print(window.shape)
     proba = clf.predict_proba(window)
     print(proba)
+
+def sendToVisTest(window) :
+    window = np.asarray([window])
+    print(window.shape)
+    proba = clf.predict_proba(window)
+    print(proba)
+    LSLoutlet.push_sample(proba[0])
+
