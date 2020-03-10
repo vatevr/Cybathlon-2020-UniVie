0c9c20413c883ac8f42833857c218c68ee34a414
MatthaeusHilpold <matthaeus.hilpold@gmail.com>
2020-03-10 17:49:55 +0100

    Added reader function to read and transform brainvision data into riemannian classifier readable format. Tested classifier, but there's still a bug

--- Pipeline/src/main/main.py
+++ Pipeline/src/main/main.py
@@ -6,23 +6,25 @@ from Pipeline.src.tools.FileReader import FileReader
 import socket
 from threading import Thread
 
-if __name__ == '__main__':
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
-    path = "../../../../tcpip/data/s01.mat"
-    clfpath="../savedFilters/test"
+
+def offline_training(datapath, savepath) :
+    reader = FileReader(datapath=datapath)
+    X, y = reader.load_brainvision()
+
+    clf = riemannianClassifier(savePath=savepath)
+
+    clf.fit(X, y)
 
 
+def online_training(path) :
 
-    #clf = riemannianClassifier(savePath=clfpath)
-    #clf.load_self()
-
-
+    clf = riemannianClassifier(savePath=path)
+    clf.load_self()
 
     simulator = UdpSimulator(fs=512)
-    #simulator.simulate_udp("../../../../tcpip/data/s01.mat", address=IPAddr, port=10005)
+    # simulator.simulate_udp("../../../../tcpip/data/s01.mat", address=IPAddr, port=10005)
     connector = UdpConnector(batch_received=sendToVisTest, n_chn=22, host="127.0.1.1", port=10005)
-    #connector.start()
+    # connector.start()
 
     broadcaster_thread = Thread(target=simulator.simulate_udp, args=(path, IPAddr, 10005))
     consumer_thread = Thread(target=connector.start, args=())
@@ -32,3 +34,19 @@ if __name__ == '__main__':
 
     broadcaster_thread.join()
     consumer_thread.join()
+
+if __name__ == '__main__':
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    path = "../../../../tcpip/data/s01.mat"
+    clfpath="../savedFilters/test"
+    kspath="../../../../recordings/KS/2020_Cybathlon_KS_Session1.vhdr"
+
+    offline_training(kspath, clfpath)
+    print("classifier trained")
+    online_training(clfpath)
+
+
+
+
+
--- Pipeline/src/tools/FileReader.py
+++ Pipeline/src/tools/FileReader.py
@@ -58,3 +58,34 @@ class FileReader():
 
         X, y, meta = MI.get_data(datasets[1], [1])
         return X, y, meta
+
+    def load_brainvision(self):
+        raw = mne.io.read_raw_brainvision(self.datapath, preload=True)
+        # Set montage (location of channels)
+        raw.rename_channels({'O9': 'I1', 'O10': 'I2'})
+        montage = mne.channels.read_montage("standard_1005")
+        raw.set_montage(montage)
+        raw.rename_channels({'I1': 'O9', 'I2': 'O10'})
+        # Remove bad channels from analysis
+        raw.info['bads'] = ["PPO9h", "FFT7h", "P10", 'Pz']
+        picks = mne.pick_types(raw.info, eeg=True, stim=False, exclude='bads')
+        raw.set_eeg_reference(ref_channels="average")
+        # Create events from triggers
+        events = mne.events_from_annotations(raw)[0]
+        events = np.asarray([events[event] for event in range(len(events[:, 0])) if (events[event][2] < 255)])
+        #print(events)
+        # events = aggregate_events(events) # Aggregate
+        tmin = 1  # time in seconds after trigger the trial should start
+        tmax = tmin + 4  # time in seconds after trigger the trial should end
+        epochs = mne.Epochs(raw, events, tmin=tmin, tmax=tmax, preload=True, baseline=None, picks=picks)
+
+        labels = epochs.events[:, -1] #1, 2, 5, 10
+        epochs = epochs.copy().crop(tmin=1., tmax=2.)
+        X = []
+        X = np.asarray([epochs.get_data()[epoch, :, :] for epoch in range(len(labels)) if (labels[epoch] == 1 or labels[epoch] == 2)])  # or 5 or 11 for 4-class
+
+        y = []
+        y = np.asarray([labels[epoch] for epoch in range(len(labels)) if (labels[epoch] == 1 or labels[epoch] == 2)])
+
+
+        return X, y
